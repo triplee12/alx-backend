f4c71bbc3cfd75d109698af661f1e917916a1464
triplee12 <tripleeoliver@gmail.com>
2023-07-25 15:57:05 +0100

    LIFO Caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""LIFO Caching."""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Last in First out caching system."""
+
+    def put(self, key, item):
+        """
+        Add an item to the cache.
+
+        Args:
+            key: The key of the item
+            item: The item to be added
+        """
+        if key is None or item is None:
+            pass
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            data_keys = [k for k in self.cache_data.keys()]
+            rm_key = data_keys.pop()
+            del self.cache_data[rm_key]
+            print(f"DISCARD: {rm_key}")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve an item from the cache.
+
+        Args:
+            key: The key of the item to be retrieved
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
