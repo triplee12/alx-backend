5f16f10360458217a85dfcd861fa351fcb426e0c
triplee12 <tripleeoliver@gmail.com>
2023-08-01 14:10:23 +0100

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Basic Babel setup."""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """i18n configuration."""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """Application entry point for i18n."""
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
